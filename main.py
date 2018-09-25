2352ac3f5163270525f7d3d3ee000d4dfe0cb859
mackbr96 <mackbr@clarkson.edu>
2018-09-25 11:44:11 -0400

    changes

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import numpy
 normalVector = []
+
+with open("train.txt") as f:
+    lines = f.readlines()
+    vectors = []
+    for i in lines:
+        vectors.append(i.split())
+        
+vectors[0].pop(len(vectors[0]) - 1)
+normalVector = vectors[0]
+vector = 
