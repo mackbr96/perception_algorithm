0f1e1215aa2abd593d5c56e3021c6de0dbe4a2c4
mackbr96 <mackbr@clarkson.edu>
2018-10-01 14:19:58 -0400

    added comments to code

--- main.py
+++ main.py
@@ -1,80 +1,76 @@
 import numpy as np
 from numpy import linalg
 normalVector = []
+def train():
+    with open("train.txt") as f:
+        lines = f.readlines()
+        vectors = []
+        for i in lines:
+            vectors.append(i.split())
 
-with open("train.txt") as f:
-    lines = f.readlines()
-    vectors = []
-    for i in lines:
-        vectors.append(i.split())
+    flags = [] #Y or N
 
-flags = []
+    for i in range(0, len(vectors)): #remove flag from vector and convert string to float
+        flags.append(vectors[i].pop())
+        for x in range(0,len(vectors[i])):
+            vectors[i][x] = float(vectors[i][x])
 
-for i in range(0, len(vectors)):
-    flags.append(vectors[i].pop())
-    for x in range(0,len(vectors[i])):
-        vectors[i][x] = float(vectors[i][x])
-
-#set up first normal vector
-if(flags[0] == "Y"):
-    normalVector = vectors[0]
-else:
-    normalVector = vectors[0]*-1
-
-
-vec = np.array(vectors)
-no = np.array(normalVector)
-no = no / linalg.norm(no)
-
-for i in range(0, len(vectors)):
-    if(flags[i] == "Y"):
-        if(np.inner(no, vec[i]) >= 0):
-            continue
-        else:
-            normal = vec[i] / linalg.norm(vec[i])
-            no = no + (normal)
-            i = 0
+    #set up first normal vector
+    if(flags[0] == "Y"):
+        normalVector = vectors[0] / linalg.norm(vectors[0])
     else:
-        if(np.inner(no, vec[i]) < 0):
-            continue
+        normalVector = vectors[0] / linalg.norm(vectors[0])*-1
+
+    vec = np.array(vectors)
+
+    for i in range(0, len(vectors)):
+        if(flags[i] == "Y"): #Yes side
+            if(np.inner(normalVector, vec[i]) >= 0): #Is good
+                continue
+            else: #Its on the wrong side
+                normal = vec[i] / linalg.norm(vec[i])
+                normalVector= normalVector + (normal)
+                i = 0
+        else: #No side
+            if(np.inner(normalVector, vec[i]) < 0): #Is good
+                continue
+            else: #Is incorrect
+                normal = vec[i] / linalg.norm(vec[i])
+                normalVector= normalVector + (-1 * normal)
+                i = 0
+    return normalVector
+
+def test(normalVector):
+    with open("test.txt") as f:
+        lines = f.readlines()
+        vectors = []
+        for i in lines:
+            vectors.append(i.split())
+
+    for i in range(0, len(vectors)): #Convert string to float in vectors
+        for x in range(0,len(vectors[i])):
+            vectors[i][x] = float(vectors[i][x])
+
+    vec = np.array(vectors)
+
+    predictions = [] #Our predictions for the test
+
+    for i in range(0, len(vectors)): 
+        if(np.inner(normalVector, vec[i]) >= 0):
+            predictions.append("Y")
         else:
-            normal = vec[i] / linalg.norm(vec[i])
-            no = no + (-1 * normal)
-            i = 0
-print(no)
+            predictions.append("N") 
+    return predictions
+
+
+
+normalVector = train()
+
+print(normalVector)
 print("FINISHED LEARNING")
 print("NOW TESTING")
 
-with open("test.txt") as f:
-    lines = f.readlines()
-    vectors = []
-    for i in lines:
-        vectors.append(i.split())
+predictions = test(normalVector)
 
-
-for i in range(0, len(vectors) ):
-    for x in range(0,len(vectors[i])):
-        vectors[i][x] = float(vectors[i][x])
-
-
-vec = np.array(vectors)
-
-solution = []
-
-for i in range(0, len(vectors)):
-    if(np.inner(no, vec[i]) >= 0):
-        solution.append("Y")
-    else:
-        solution.append("N") 
-
-
-print(solution)
+print(predictions)
 print("Finished")
-
-
-
-
-
-
-
-    
